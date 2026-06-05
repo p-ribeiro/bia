6fbc1a1dfabb72cdaf63039187ccfd2005dcde2a
Pedro Ribeiro <pedromr21@gmail.com>
2026-06-05 17:55:18 -0400

    Update user_data_ec2_zona_a.sh
    removing swap

--- scripts/user_data_ec2_zona_a.sh
+++ scripts/user_data_ec2_zona_a.sh
@@ -26,21 +26,12 @@ sudo mkdir -p /usr/local/lib/docker/cli-plugins
 sudo curl -SL https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-linux-x86_64 -o /usr/local/lib/docker/cli-plugins/docker-compose
 sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
 
-
-#Adicionar swap
-sudo dd if=/dev/zero of=/swapfile bs=128M count=32
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
-sudo swapon /swapfile
-sudo echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
-
-
 #Instalar node e npm
 curl -fsSL https://rpm.nodesource.com/setup_21.x | sudo bash -
 sudo yum install -y nodejs
 
-#Configurar python 3.11 e uv para uso com mcp servers da aws
-sudo dnf install python3.11 -y
+#Configurar python 3.13 e uv para uso com mcp servers da aws
+sudo dnf install python3.13 -y
 
 sudo -u ec2-user bash -c 'curl -LsSf https://astral.sh/uv/install.sh | sh'
 echo 'export PATH="$HOME/.local/bin:$PATH"' >> /home/ec2-user/.bashrc
